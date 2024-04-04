adfac71271e095131000ebd293f74b9eb98b5721
perismuthoni <muthonimuriuki22@gmail.com>
2024-04-04 13:44:14 +0000

    flask cors version

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask.ext.session import Session
 
-
-
 SESSION_TYPE = 'memcache'
 
 app = Flask(__name__)
@@ -16,8 +14,6 @@ migrate = Migrate(app, db)
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 sess = Session()
 
-
-
 if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
     sess.init_app(app)
-    app.run(debug=True)
